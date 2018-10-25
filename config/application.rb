39560ac3378596338f4a51c116c56e89e0c51b9a
kikuchi <usushio193@gmail.com>
2018-10-25 19:00:03 +0900

    設定等の変更

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,10 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Allow-Origin' => 'http://localhost:4000',
+      'Access-Control-Request-Method' => '*'
+    }
   end
 end
